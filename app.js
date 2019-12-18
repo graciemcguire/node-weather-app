dd38d14d76be890ad5b33e1734c960df8496ada2
Gracie <m.graciemcguire@gmail.com>
2019-12-18 13:57:01 -0500

    fix lat and long

--- app.js
+++ app.js
@@ -6,12 +6,9 @@ request({ url: url, json: true }, (error, response) => {
 
   console.log(`${ response.body.daily.data[0].summary } it is currently ${ temperature } out,there is a ${ precipProbability }% chance of rain`);
 })
-//get lat/long
+
 request({ url: geoCodeURL, json: true }, (error, response) => {
-  console.log(response.body.features[0].place_name);
-  console.log(response.body.features[0].center);
-  const latitude = response.body.features[0].center[1]
   const longitude = response.body.features[0].center[0]
-  console.log(latitude);
-  console.log(longitude);
+  const latitude = response.body.features[0].center[1]
+  console.log('lat:', latitude, 'long:', longitude);
 })
