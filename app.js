00c4a8ae12210a979026d32eddd5d97f8ec0e4fe
Gracie <m.graciemcguire@gmail.com>
2020-05-01 13:52:13 -0600

    utils directory

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const address = process.argv[2]
-=======
-const request = require('request')
-const url = 'https://api.darksky.net/forecast/37e5df372f2e74b6cbb3c923555374c9/37.8267,-122.4233'
->>>>>>> 286213d9e61e002a86e0a1069c0bb92877c352cf
 
 if (!address) {
     console.log('Please provide an address')
@@ -16,7 +11,6 @@ if (!address) {
             return console.log(error)
         }
 
-<<<<<<< HEAD
         forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
                 return console.log(error)
@@ -27,17 +21,3 @@ if (!address) {
         })
     })
 }
-=======
-  if (error){
-    console.log('unable to connect');
-  } else {
-    console.log(`${ response.body.daily.data[0].summary } it is currently ${ temperature } out,there is a ${ precipProbability }% chance of rain`);
-  }
-})
-
-// request({ url: geoCodeURL, json: true }, (error, response) => {
-//   const longitude = response.body.features[0].center[0]
-//   const latitude = response.body.features[0].center[1]
-//   console.log('lat:', latitude, 'long:', longitude);
-// })
->>>>>>> 286213d9e61e002a86e0a1069c0bb92877c352cf
